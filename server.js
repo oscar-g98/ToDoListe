0dc3047c2c7495ff16e06e49ed7be84bcbe0cf94
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-24 12:57:50 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,6 @@ app.use("/JS", express.static('./JS/'));
 app.use(express.static('public'));
 app.use(express.json());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'front_end', 'build')));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, 'front_end', 'build', 'index.html'))
-    });
-    
-  }
 
 var port = process.env.PORT||3000;
 app.listen(port, function(){
