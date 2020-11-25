33237b2f1b307a52546303688e1771d1c36eee87
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-25 16:02:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ app.set('port', port);
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-const port = (process.env.PORT||3000);
 
-app.listen(app.get('port'), function () {
-  console.log('server running', app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
