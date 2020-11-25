144f20c94c86e7888b3c1b96fffc7f2705560ce0
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-25 21:31:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(bodyParser.json());
 
 const port = (process.env.PORT||3000);
 
-app.listen(app.get('port'), function () {
-  console.log('server running', app.get('port'));
+app.listen(port, function(){
+    console.log("server listening on port 3000!");
 });
