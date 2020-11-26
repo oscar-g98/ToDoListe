4cf91e8df0c11ad4875a6ad53f0b2ceeece2b73d
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-26 15:00:16 +0100

    Update routes.js

--- routes.js
+++ routes.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 
-router.get("/JS", function(req, res) {
+router.get("/to-do-list", function(req, res) {
    // console.log("hello");
 res.render("index");
 });
