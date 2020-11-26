b741bf8e78c277f51f7a3fd2b9fcb0b903dc3daf
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-26 14:51:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const express = require("express");
 const {client} = require("pg")
 const app = express();
 const port = process.env.PORT||3000;
+const routes = require("./routes");
 
 app.use("/JS", express.static('./JS/'));
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.set('port', port);
+app.use(routes);
 
 app.listen(app.get('port'), function () {
     console.log('server running', app.get('port'));
   });
-
- 
