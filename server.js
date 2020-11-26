635679d239f1efb23ad00b8a257548b6c0bbe61e
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-26 13:39:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const bodyParser = require('body-parser');
 const express = require("express");
 const {client} = require("pg")
 const app = express();
+const port = process.env.PORT||3000;
 
 app.use("/JS", express.static('./JS/'));
-
 app.use(express.static('public'));
 app.use(bodyParser.json());
-
-
-const port = process.env.PORT||3000;
 app.set('port', port);
+
 app.listen(app.get('port'), function () {
     console.log('server running', app.get('port'));
   });
+
+  
