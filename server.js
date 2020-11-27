205bc9e4e7c6556918427d7c4aa3465e9a3cb60f
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-27 13:15:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 express()
   .use("/JS", express.static('./JS'))
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'Public')))
   .set('views', path.join(__dirname, 'server'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
