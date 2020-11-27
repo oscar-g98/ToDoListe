406cf1b5bd713070def10c15904f8c726bda7566
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-27 13:06:42 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const fs = require("fs");
-const bodyParser = require('body-parser');
-const {client} = require("pg")
-const express = require("express");
-const server= express();
+const express = require('express');
+const bodyparser = require("body-parser");
+const {client} = require("pg");
+const server = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 
+
 express()
+  .use("/JS", express.static('./JS'))
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'server'))
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
