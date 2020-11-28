edfdf21e52cd7552891d1cb29cd520a7fc9cd89a
Torstb18 <70886231+Torstb18@users.noreply.github.com>
2020-11-28 13:44:58 +0100

    Update server.js
    med start på database

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
 const fs = require("fs");
-const express = require('express');
-const bodyparser = require("body-parser");
-const {client} = require("pg");
-const server = express();
+const bodyParser = require('body-parser');
+const {Client} = require("pg");
+const express = require("express");
+const server= express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 
-
 express()
-  .use("/JS", express.static('./JS'))
-  .use(express.static(path.join(__dirname, 'Public')))
+  .use ("/JS", express.static("./JS"))
+  .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'server'))
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)); 
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+  const client = new Client({
+    user: "dudkpuijfzevxz",
+    password: "65cfb20a08ce96cb267841a0bc36a1e2510dfd5f16b22bf01b337b0d0f4e096d",
+    host: "postgres://dudkpuijfzevxz:65cfb20a08ce96cb267841a0bc36a1e2510dfd5f16b22bf01b337b0d0f4e096d@ec2-46-137-124-19.eu-west-1.compute.amazonaws.com:5432/d6c72hpen6sbb1",
+    port: "5432",
+    database: "d6c72hpen6sbb1"
+  })
+  
+  client.connect()
+  .then(()=> console.log("Connected"))
+  .catch(error => console.log("ERROR",error.message || error))
+  .finally(() => client.end())
