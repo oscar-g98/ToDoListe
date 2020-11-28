80aa8b53f4d93f4ce8e1f67cea42140585ae774b
oscar-g98 <70950696+oscar-g98@users.noreply.github.com>
2020-11-28 15:41:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,22 @@ const PORT = process.env.PORT || 3000;
 
 express()
   .use ("/JS", express.static("./JS"))
-  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.static(path.join(__dirname, 'Public')))
   .set('views', path.join(__dirname, 'server'))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 
   
+const client = new Client({
+    user: "dudkpuijfzevxz",
+    password: "65cfb20a08ce96cb267841a0bc36a1e2510dfd5f16b22bf01b337b0d0f4e096d",
+    host: "postgres://dudkpuijfzevxz:65cfb20a08ce96cb267841a0bc36a1e2510dfd5f16b22bf01b337b0d0f4e096d@ec2-46-137-124-19.eu-west-1.compute.amazonaws.com:5432/d6c72hpen6sbb1",
+    port: "5432",
+    database: "d6c72hpen6sbb1"
+  })
+  
+  client.connect()
+  .then(()=> console.log("Connected"))
+  .catch(error => console.log("ERROR",error.message || error))
+  .finally(() => client.end())
